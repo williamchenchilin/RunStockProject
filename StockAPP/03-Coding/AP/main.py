e5d870370fa31de0a7c0ea0ebe6a4a76ac7fd279
williamchenchilin <williamchenchilin@gmail.com>
2024-01-16 23:18:05 +0800

    新增主要程式
    新增主要程式

--- StockAPP/03-Coding/AP/main.py
+++ StockAPP/03-Coding/AP/main.py
@@ -6,7 +6,8 @@
 # 備註     : 
 #******************************************************************
 # 日期          編輯人員              變更原因
-# 2024/01/01    William              初始版本
+# 2024/01/01    William              建立範本
+# 2024/01/16    William              新增主要line程式
 ******************************************************************
 '''
 # ----------------------
@@ -15,12 +16,12 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage, LocationSendMessage
 from datetime import datetime
 
-import pyodbc
-import schedule
-import time
+#import pyodbc
+#import schedule
+#import time
 
 # ----------------------
 # 設定程式運行
@@ -46,6 +47,10 @@ DEL_CNT = 0                          # 刪除資料筆數
 ErrorMessage = ''       # 錯誤訊息
 ErrorSeverity = 0       # 錯誤嚴重程度
 ErrorState = 0          # 錯誤狀態
+CAT = 'NKbDp9O6/M2EMnRFcOJcZjZfByb+7cPd3E1YPVm2BycjQ/yUhtyPEVDR9U3khmiFkeY7LhssAN+ucYQCczEye+7Fu+80dB+waO2DgxQO41I52NfooWS7UUKcsPmcnY6hYfkbvv1Q1YCI8+QUPKiyPgdB04t89/1O/w1cDnyilFU='
+CS='c9031d7e26c12cf1388a8664bedfdf79'
+line_bot_api = LineBotApi(CAT)
+handler = WebhookHandler(CS)
 
 # ------------------------------
 # 設定必輸入變數
@@ -66,11 +71,44 @@ UPD_CNT = 0                     # 更新資料筆數
 DEL_CNT = 0                     # 刪除資料筆數
 # OUT 回傳值
 PARAM = PARAM if PARAM is not None else ''
+# ----------------------
+# 程式開始
+# ----------------------
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 取得 Line 訊息的 X-Line-Signature Header
+    signature = request.headers['X-Line-Signature']
 
+    # 取得 POST 資料
+    body = request.get_data(as_text=True)
 
+    # 記錄 LINE 的事件
+    app.logger.info("Request body: " + body)
 
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
+    return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 獲取使用者相關資訊
+    user_id = event.source.user_id
+    source_type = event.source.type
+    message_text = event.message.text
+
+    # 根據不同的使用者進行回覆
+    if source_type == "user":
+        reply_text = f"這是來自使用者 {user_id} 的訊息: {message_text}"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+    elif source_type == "group":
+        reply_text = f"這是來自群組 {user_id} 的訊息: {message_text}"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+    elif source_type == "room":
+        reply_text = f"這是來自聊天室 {user_id} 的訊息: {message_text}"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 
 # ----------------------
@@ -78,6 +116,6 @@ PARAM = PARAM if PARAM is not None else ''
 # ----------------------
 if __name__ == "__main__":
     app.run(port=5000)
-    while True:
+'''    while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(1)'''
